300851a02b4dd6c0a835e695c93083f8fbc886f8
chanchal goyal <miss71910@gmail.com>
2024-09-13 18:13:52 +0530

    Update .gitignore and add untracked files

---
+++ store/index.js
@@ -0,0 +1,39 @@
+import process from 'process';
+
+import { combineReducers, createStore } from "redux";
+import productsReducer from "./productsReducer";
+import cartReducer, {
+  addCartItem,
+  decreaseCartItemQuantity,
+  increaseCartItemQuantity,
+  decreaseCartItemQuantity,
+} from "./cartReducer";
+import wishListReducer, {
+  addWishListItem,
+  removeWishListItem,
+} from "./wishListReducer";
+
+const reducer = combineReducers({
+  products: productsReducer,
+  cartItems: cartReducer,
+  wishList: wishListReducer,
+});
+
+export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
+
+// console.log(store);
+
+// store.dispatch(addCartItem(1));
+// store.dispatch(addCartItem(12));
+
+// store.dispatch(increaseCartItemQuantity(12))
+// store.dispatch( decreaseCartItemQuantity(12));
+
+// store.dispatch( decreaseCartItemQuantity(12));
+
+
+// store.dispatch(addWishListItem(18))
+// store.dispatch(addWishListItem(11))
+
+// store.dispatch(removeWishListItem(11))
+// store.dispatch(removeWishListItem(18))
---
+++ store/productsReducer.js
@@ -0,0 +1,5 @@
+import { productsList } from "./productsLists";
+
+export default function productsReducer(state = productsList) {
+  return state;
+}
